4ba75159dbffbd3f7bef5b8947da8a21257b713c
Jose Segura <josed.segurav@gmail.com>
2025-07-14 11:55:56 -0500

    Added kaminari for pagination, updated artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
     def index
-        @artists = Artist.all
-        @albums = Artist.
+        @artists = Artist.includes(:albums, :tracks).all
+        @artists = @artists.page(params[:page]).per(20)
+
     end
 end
